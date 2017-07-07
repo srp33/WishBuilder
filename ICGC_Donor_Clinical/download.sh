be07c3a9ba22928f4f08c2787ab6ad801255645e
Stephen Piccolo <goldfish1434@yahoo.com>
2017-07-06 22:26:34 -0600

    Added example of modifying .gitignore

--- ICGC_Donor_Clinical/download.sh
+++ ICGC_Donor_Clinical/download.sh
@@ -4,3 +4,5 @@ url="https://dcc.icgc.org/api/v1/download?fn=/current/Summary/donor.all_projects
 fileName="donor.all_projects.tsv.gz"
 
 curl -o "$fileName" -L "$url"
+
+echo "$fileName" >> .gitignore
