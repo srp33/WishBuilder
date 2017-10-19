44405fcd26efc1a3a820e6d26e2436d14bb4ec38
glenrs <grexsumsion@gmail.com>
2017-10-19 11:41:11 -0600

    BiomarkerBenchmark_GSE2109_Colon finished

---
+++ BiomarkerBenchmark_GSE2109_Colon/install.sh
@@ -0,0 +1,10 @@
+#! bin/bash
+
+cp ../Helper/BiomarkerBenchmark/download.sh .
+cp ../Helper/BiomarkerBenchmark/description.md .
+cp ../Helper/BiomarkerBenchmark/parse.py .
+cp ../Helper/BiomarkerBenchmark/parse.sh .
+cp ../Helper/BiomarkerBenchmark/cleanup.sh .
+
+sed -e "s,{urlExpression},https://osf.io/yh5eb/download?version=1,g" -e "s,{urlClinical},https://osf.io/jczdb/download?version=1,g" ../Helper/BiomarkerBenchmark/download.sh > download.sh 
+
