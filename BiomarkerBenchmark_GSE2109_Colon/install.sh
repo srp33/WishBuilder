666efaf2b85a5c0fbed23c29bbf351c3646ee51d
glenrs <grexsumsion@gmail.com>
2017-10-19 13:29:54 -0600

    fixed install.sh

--- BiomarkerBenchmark_GSE2109_Colon/install.sh
+++ BiomarkerBenchmark_GSE2109_Colon/install.sh
@@ -1,7 +1,6 @@
-#! bin/bash
+#! /bin/bash
 
 cp ../Helper/BiomarkerBenchmark/download.sh .
-cp ../Helper/BiomarkerBenchmark/description.md .
 cp ../Helper/BiomarkerBenchmark/parse.py .
 cp ../Helper/BiomarkerBenchmark/parse.sh .
 cp ../Helper/BiomarkerBenchmark/cleanup.sh .
