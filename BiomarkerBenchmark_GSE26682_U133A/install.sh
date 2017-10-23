9a55efdb6ce3b0e435dcdd86ada659fa45a3e402
glenrs <grexsumsion@gmail.com>
2017-10-23 10:39:12 -0600

    BiomarkerBenchmark_GSE26682_U133A finished

---
+++ BiomarkerBenchmark_GSE26682_U133A/install.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+cp ../Helper/BiomarkerBenchmark/download.sh .
+cp ../Helper/BiomarkerBenchmark/parse.py .
+cp ../Helper/BiomarkerBenchmark/parse.sh .
+cp ../Helper/BiomarkerBenchmark/cleanup.sh .
+
+sed -e "s,{urlExpression},https://osf.io/m46nu/download,g" -e "s,{urlClinical},https://osf.io/75j6x/download,g" ../Helper/BiomarkerBenchmark/download.sh > download.sh 
+
