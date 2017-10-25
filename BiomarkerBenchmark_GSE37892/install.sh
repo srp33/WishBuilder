161e9587e1c798115b5cf6ccb5760f80d5c1672d
glenrs <grexsumsion@gmail.com>
2017-10-25 10:40:56 -0600

    BiomarkerBenchmark_GSE37892 finished

---
+++ BiomarkerBenchmark_GSE37892/install.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+cp ../Helper/BiomarkerBenchmark/download.sh .
+cp ../Helper/BiomarkerBenchmark/parse.py .
+cp ../Helper/BiomarkerBenchmark/parse.sh .
+cp ../Helper/BiomarkerBenchmark/cleanup.sh .
+
+sed -e "s,{urlExpression},https://osf.io/jw7yv/download,g" -e "s,{urlClinical},https://osf.io/jhxk2/download,g" ../Helper/BiomarkerBenchmark/download.sh > download.sh 
+
