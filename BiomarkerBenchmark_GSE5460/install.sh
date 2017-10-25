eca54a817d3ee12d527013cde9860157576601ad
glenrs <grexsumsion@gmail.com>
2017-10-25 10:50:48 -0600

    BiomarkerBenchmark_GSE5460 finished

---
+++ BiomarkerBenchmark_GSE5460/install.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+cp ../Helper/BiomarkerBenchmark/download.sh .
+cp ../Helper/BiomarkerBenchmark/parse.py .
+cp ../Helper/BiomarkerBenchmark/parse.sh .
+cp ../Helper/BiomarkerBenchmark/cleanup.sh .
+
+sed -e "s,{urlExpression},https://osf.io/k4p8c/download,g" -e "s,{urlClinical},https://osf.io/5cua4/download,g" ../Helper/BiomarkerBenchmark/download.sh > download.sh 
+
