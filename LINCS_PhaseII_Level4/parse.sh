33725cf06da9521e5593f719bfba456cc3a60979
glenrs <grexsumsion@gmail.com>
2017-11-29 16:17:38 -0700

    fixed parse.sh

--- LINCS_PhaseII_Level4/parse.sh
+++ LINCS_PhaseII_Level4/parse.sh
@@ -7,42 +7,26 @@
 
 #Directory variables
 redirectedTempFolder=tmp
-createdFilesFolder=$redirectedTempFolder/Files_Created
-metaDataOutFiles=$createdFilesFolder/Meta_Data
-expressionDataOutFiles=$createdFilesFolder/Expression_Data
 softwareFolder=Software
 
 #Downloaded in download.sh
-sigFileName=$redirectedTempFolder/GSE70138_Broad_LINCS_sig_info_2017-03-06.txt.gz
-gctxFileName=$redirectedTempFolder/LINCS_PhaseII_Level5.gctx
+instInfoFileName=$redirectedTempFolder/GSE70138_Broad_LINCS_inst_info.txt.gz
+gctxFileName=$redirectedTempFolder/LINCS_PhaseII_Level4.gctx
 geneFile=$redirectedTempFolder/GSE70138_Broad_LINCS_gene_info_2017-03-06.txt.gz
 
 #Downloaded and Installed in install.sh
 minicondaPath=$softwareFolder/miniconda/bin/
-parallelCommand=$softwareFolder/parallel-20170922/src/parallel
 
 #Will Create
-tempSigFileName=$createdFilesFolder/GSE70138_Broad_LINCS_sig_info_2017-03-06_adjusted.txt.gz
-parallelFile=$createdFilesFolder/LINCS_PhaseII_Level5_parallel.txt
-metaDataOutFilePrefix=$metaDataOutFiles/LINCS_PhaseII_Level5_sig_id_group_metadata_
-expressionDataOutFilePrefix=$expressionDataOutFiles/LINCS_PhaseII_Level5_sig_id_group_expression_data_
-tmpHeaderFile=$createdFilesFolder/header.txt
 dataOutFile=data.tsv.gz
 metadataOutFile=metadata.tsv.gz
 
 #Other Metadata files that are included in final output
 cellInfo=$redirectedTempFolder/GSE70138_Broad_LINCS_cell_info_2017-04-28.txt.gz
 pertInfo=$redirectedTempFolder/GSE70138_Broad_LINCS_pert_info_2017-03-06.txt.gz
-sigMetrics=$redirectedTempFolder/GSE70138_Broad_LINCS_sig_metrics_2017-03-06.txt.gz
 
 rm -f $metadataOutFile
 rm -f $dataOutFile
-
-#Create createdFilesFolder
-mkdir -p $createdFilesFolder
-rm -rf $createdFilesFolder/*
-mkdir -p $metaDataOutFiles
-mkdir -p $expressionDataOutFiles
 
 #miniconda is used to store panda software in environments need to activate environment
 echo "Setting up environment"
@@ -50,4 +34,4 @@ cd $minicondaPath
 source activate lincs_env
 cd ../../..
 
-python parse.py $sigFileName $gctxFileName $metadataOutFile $dataOutFile $geneFile $cellInfo $pertInfo $sigMetrics
+python parse.py $instInfoFileName $gctxFileName $metadataOutFile $dataOutFile $geneFile $cellInfo $pertInfo 
