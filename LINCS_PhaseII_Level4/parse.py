52e598ba9e9647d53116c5057b2e0a44530063a7
glenrs <grexsumsion@gmail.com>
2017-11-30 14:21:52 -0700

    fixed parse.py, also only included lines that are in both expression and metadata files

--- LINCS_PhaseII_Level4/parse.py
+++ LINCS_PhaseII_Level4/parse.py
@@ -2,14 +2,13 @@ import h5py
 import numpy as np
 import sys, gzip
 
-sigInfoFile = sys.argv[1]
+instInfoFile = sys.argv[1]
 gctxFileName = sys.argv[2]
 metadataOut = sys.argv[3]
 dataOut = sys.argv[4]
 geneFile = sys.argv[5]
 cellInfo = sys.argv[6]
 pertInfo = sys.argv[7]
-sigMetrics = sys.argv[8]
 
 f = h5py.File(gctxFileName, "r")
 
@@ -31,23 +30,6 @@ with gzip.open(geneFile, 'r') as f :
     for line in f :
         list = line.strip('\n').split('\t')
         geneDict[list[0]] = list[1]
-
-print("writing expression file")
-
-f = gzip.open(dataOut, 'w')
-try :
-    f.write("")
-    for value in colgrp["id"] :
-        f.write('\t' + geneDict[value])
-    f.write('\n')
-
-    index = 0
-    for line in subsubgrpData["matrix"] :
-        a = np.asarray(line).astype(str)
-        f.write(rowgrp["id"][index] + '\t' + '\t'.join(a) + '\n')
-        index = index + 1 
-finally :
-    f.close()
 
 
 print("writing metadata file")
@@ -74,53 +56,58 @@ try :
 finally:
     f.close()
 
-sigMetricsHeaderList = []
-sigMetricsDict = {}
-f = gzip.open(sigMetrics, 'r')
-try:
-    firstLineList = f.readline().strip('\n').split('\t')
-    sigMetricsHeaderList = firstLineList[2:4] + firstLineList[7:]
-    for line in f :
-        lineList = line.strip('\n').split('\t')
-        sigMetricsDict[lineList[1]] = lineList[2:4] + lineList[7:]
-finally:
-    f.close()
-
-sigInfo =  gzip.open(sigInfoFile, 'r')
+instInfo =  gzip.open(instInfoFile, 'r')
 metaOut = gzip.open(metadataOut, 'w')
+instInfoSampleIds = []
 try:
-    headerList = sigInfo.readline().strip('\n').split('\t')
+    headerList = instInfo.readline().strip('\n').split('\t')
     sigId = ""
     metaOut.write("Sample\tVariable\tValue\n")
-    for row in sigInfo :
-        rowList = row.strip('\n').split('\t')[:7]
+    for row in instInfo :
+        rowList = row.strip('\n').split('\t')
+        instInfoSampleIds.append(rowList[0])
+        if rowList[0] not in rowgrp["id"] :
+            break
         for i in range(len(rowList) - 1 ):
-            if(str(rowList[i + 1]) != "-666") :
+            if(str(rowList[i + 1]) != "-666") and (str(rowList[i + 1]) != "-666.0") :
                 metaOut.write(str(rowList[0]) + '\t' + str(headerList[i + 1]) + '\t' + str(rowList[i + 1]) + '\n')
                 
-                if (rowList[0] != sigId) :
-                    sigMetricsList = sigMetricsDict[rowList[0]] ##This will add the sigMetrics metadata to the metadata.tsv.gz 
-                    for j in range(len(sigMetricsList)) :
-                        if(str(sigMetricsList[j]) != "-666") :
-                            metaOut.write(str(rowList[0]) + '\t' + str(sigMetricsHeaderList[j]) + '\t' + str(sigMetricsList[j]) + '\n')
-                    sigId = rowList[0]
-
-                if (headerList[i + 1] == "cell_id") :
-                    try :
-                        cellIdList = cellInfoDict[rowList[i + 1]] ##This will add the cellInfo to the metadata.tsv.gz
-                        for j in range(len(cellIdList)) :
-                            if(str(cellIdList[j]) != "-666") :
-                                metaOut.write(str(rowList[0]) + '\t' + str(cellHeaderList[j]) + '\t' + str(cellIdList[j]) + '\n')
-                    except :
-                        continue ## This catches SNUC4 that doesn't have any cellInfo, but is included in the sigInfo file
-                elif (headerList[i + 1] == "pert_id") :
-                    try :
-                        pertInfoIdList = pertInfoDict[rowList[i + 1]] ##This will add the pertInfo metadata to the metadata.tsv.gz
-                        for j in range(len(pertInfoIdList)) :
-                            if(str(pertInfoIdList[j]) != "-666") :
-                                metaOut.write(str(rowList[0]) + '\t' + str(pertInfoHeaderList[j]) + '\t' + str(pertInfoIdList[j]) + '\n')
-                    except :
-                        continue
+            if (headerList[i + 1] == "cell_id") :
+                try :
+                    cellIdList = cellInfoDict[rowList[i + 1]] ##This will add the cellInfo to the metadata.tsv.gz
+                    for j in range(len(cellIdList)) :
+                        if(str(cellIdList[j]) != "-666") :
+                            metaOut.write(str(rowList[0]) + '\t' + str(cellHeaderList[j]) + '\t' + str(cellIdList[j]) + '\n')
+                except :
+                    continue ## This catches SNUC4 that doesn't have any cellInfo, but is included in the sigInfo file
+            elif (headerList[i + 1]  == "pert_id") :
+                try :
+                    pertInfoIdList = pertInfoDict[rowList[i + 1]] ##This will add the pertInfo metadata to the metadata.tsv.gz
+                    for j in range(len(pertInfoIdList)) :
+                        if(str(pertInfoIdList[j]) != "-666") :
+                            metaOut.write(str(rowList[0]) + '\t' + str(pertInfoHeaderList[j]) + '\t' + str(pertInfoIdList[j]) + '\n')
+                except :
+                    continue
 finally:
-    sigInfo.close()
+    instInfo.close()
     metaOut.close()
+
+
+print("writing expression file")
+
+f = gzip.open(dataOut, 'w')
+try :
+    f.write("Sample")
+    for value in colgrp["id"] :
+        f.write('\t' + geneDict[value])
+    f.write('\n')
+
+    index = 0
+    for line in subsubgrpData["matrix"] :
+        a = np.asarray(line).astype(str)
+        if rowgrp["id"][index] not in instInfoSampleIds :
+            break 
+        f.write(rowgrp["id"][index] + '\t' + '\t'.join(a) + '\n')
+        index = index + 1 
+finally :
+    f.close()
