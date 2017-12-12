4bca66b86fab7dfb87abcedd5355dc37224f2972
Kimball Hill <hillkimball@gmail.com>
2017-12-12 08:43:04 -0700

    updated clinical data url

--- BiomarkerBenchmark_GSE2109_Prostate/install.sh
+++ BiomarkerBenchmark_GSE2109_Prostate/install.sh
@@ -5,5 +5,5 @@ cp ../Helper/BiomarkerBenchmark/parse.py .
 cp ../Helper/BiomarkerBenchmark/parse.sh .
 cp ../Helper/BiomarkerBenchmark/cleanup.sh .
 
-sed -e "s,{urlExpression},https://osf.io/jcqgw/download,g" -e "s,{urlClinical},https://osf.io/zdmk7/download,g" ../Helper/BiomarkerBenchmark/download.sh > download.sh 
+sed -e "s,{urlExpression},https://osf.io/jcqgw/download,g" -e "s,{urlClinical},https://osf.io/q7yxc/download,g" ../Helper/BiomarkerBenchmark/download.sh > download.sh 
 
