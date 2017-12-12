38fccf9a59c3e2411aa83e1dfc650b019d24d1b9
Kimball Hill <hillkimball@gmail.com>
2017-12-12 08:42:09 -0700

    updated clinical data url

--- BiomarkerBenchmark_GSE2109_Uterus/install.sh
+++ BiomarkerBenchmark_GSE2109_Uterus/install.sh
@@ -5,5 +5,5 @@ cp ../Helper/BiomarkerBenchmark/parse.py .
 cp ../Helper/BiomarkerBenchmark/parse.sh .
 cp ../Helper/BiomarkerBenchmark/cleanup.sh .
 
-sed -e "s,{urlExpression},https://osf.io/g6thp/download,g" -e "s,{urlClinical},https://osf.io/m6gve/download,g" ../Helper/BiomarkerBenchmark/download.sh > download.sh 
+sed -e "s,{urlExpression},https://osf.io/g6thp/download,g" -e "s,{urlClinical},https://osf.io/dpn9z/download,g" ../Helper/BiomarkerBenchmark/download.sh > download.sh 
 
