a089ffd142fbefc4f1aa19d2bc3f4bf99726bcbf
Kimball Hill <hillkimball@gmail.com>
2017-12-12 08:43:57 -0700

    updated clinical data url

--- BiomarkerBenchmark_GSE2109_Ovary/install.sh
+++ BiomarkerBenchmark_GSE2109_Ovary/install.sh
@@ -5,5 +5,5 @@ cp ../Helper/BiomarkerBenchmark/parse.py .
 cp ../Helper/BiomarkerBenchmark/parse.sh .
 cp ../Helper/BiomarkerBenchmark/cleanup.sh .
 
-sed -e "s,{urlExpression},https://osf.io/8quky/download,g" -e "s,{urlClinical},https://osf.io/z6q59/download,g" ../Helper/BiomarkerBenchmark/download.sh > download.sh 
+sed -e "s,{urlExpression},https://osf.io/8quky/download,g" -e "s,{urlClinical},https://osf.io/gsc8d/download,g" ../Helper/BiomarkerBenchmark/download.sh > download.sh 
 
