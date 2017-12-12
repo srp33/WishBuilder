2a9630bb2fe9269c0c5739398e3d76953d0798e2
Kimball Hill <hillkimball@gmail.com>
2017-12-12 08:52:34 -0700

    updated clinical data url

--- BiomarkerBenchmark_GSE10320/install.sh
+++ BiomarkerBenchmark_GSE10320/install.sh
@@ -5,5 +5,5 @@ cp ../Helper/BiomarkerBenchmark/parse.py .
 cp ../Helper/BiomarkerBenchmark/parse.sh .
 cp ../Helper/BiomarkerBenchmark/cleanup.sh .
 
-sed -e "s,{urlExpression},https://osf.io/2dbhv/download?version=1,g" -e "s,{urlClinical},https://osf.io/7yztd/download?version=1,g" ../Helper/BiomarkerBenchmark/download.sh > download.sh 
+sed -e "s,{urlExpression},https://osf.io/2dbhv/download?version=1,g" -e "s,{urlClinical},https://osf.io/39yz4/download?version=1,g" ../Helper/BiomarkerBenchmark/download.sh > download.sh 
 
