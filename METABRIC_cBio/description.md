f6c0c611a9341434e710848bc3eb29071d53a773
Brandon Fry <frytime32@gmail.com>
2018-02-01 11:29:48 -0700

    First commit for completed scripts

---
+++ METABRIC_cBio/description.md
@@ -0,0 +1,1 @@
+This descrpition will be written soon
---
+++ METABRIC_cBio/parse.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Make sure the whole script will fail if any part of it fails
+set -euo pipefail
+
+python3 parse_metadata.py "./brca_metabric/data_clinical_sample.txt" "./brca_metabric/data_clinical_patient.txt" "./brca_metabric/data_CNA.txt" "./brca_metabric/data_mutations_extended.txt" "metadata.tsv.gz"
+python3 parse_data.py "./brca_metabric/data_expression.txt" "data.tsv.gz"
+python3 keep_common_samples.py "metadata.tsv.gz" "data.tsv.gz"
