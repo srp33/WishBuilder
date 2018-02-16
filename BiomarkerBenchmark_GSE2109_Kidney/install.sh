48038571f03d9740088c56ac98f0ec1a15289294
Benjamin Cookson <btc36@m7int02.fsl.byu.edu>
2018-02-16 16:58:19 -0700

    Updated Description

--- BiomarkerBenchmark_GSE2109_Kidney/install.sh
+++ BiomarkerBenchmark_GSE2109_Kidney/install.sh
@@ -5,5 +5,4 @@ cp ../Helper/BiomarkerBenchmark/parse.py .
 cp ../Helper/BiomarkerBenchmark/parse.sh .
 cp ../Helper/BiomarkerBenchmark/cleanup.sh .
 
-sed -e "s,{urlExpression},https://osf.io/3dkyv/download,g" -e "s,{urlClinical},https://osf.io/a9swr/download,g" ../Helper/BiomarkerBenchmark/download.sh > download.sh 
-
+sed -e s,{urlExpression},https://osf.io/bu6tk/download?version=4,g  ../Helper/BiomarkerBenchmark/download.sh > download.sh
