8a71a98c1a023f5191b2acec68076ba4eb11e1ce
Benjamin Cookson <btc36@m7int02.fsl.byu.edu>
2018-02-16 16:41:04 -0700

    Updated Descrition

--- BiomarkerBenchmark_GSE2109_Endometrium/install.sh
+++ BiomarkerBenchmark_GSE2109_Endometrium/install.sh
@@ -5,5 +5,4 @@ cp ../Helper/BiomarkerBenchmark/parse.py .
 cp ../Helper/BiomarkerBenchmark/parse.sh .
 cp ../Helper/BiomarkerBenchmark/cleanup.sh .
 
-sed -e "s,{urlExpression},https://osf.io/6avdw/download,g" -e "s,{urlClinical},https://osf.io/9zan5/download,g" ../Helper/BiomarkerBenchmark/download.sh > download.sh 
-
+sed -e s,{urlExpression},https://osf.io/gnkem/download?version=4,g  ../Helper/BiomarkerBenchmark/download.sh > download.sh
