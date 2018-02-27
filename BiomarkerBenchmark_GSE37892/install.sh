78fb0bd324dea27c3819639f7cad583e673237f9
Benjamin Cookson <btc36@m8int02.fsl.byu.edu>
2018-02-27 16:14:55 -0700

    updated to new version on OSF

--- BiomarkerBenchmark_GSE37892/install.sh
+++ BiomarkerBenchmark_GSE37892/install.sh
@@ -5,5 +5,4 @@ cp ../Helper/BiomarkerBenchmark/parse.py .
 cp ../Helper/BiomarkerBenchmark/parse.sh .
 cp ../Helper/BiomarkerBenchmark/cleanup.sh .
 
-sed -e "s,{urlExpression},https://osf.io/jw7yv/download,g" -e "s,{urlClinical},https://osf.io/bckt5/download,g" ../Helper/BiomarkerBenchmark/download.sh > download.sh 
-
+sed -e s,{urlExpression},https://osf.io/j6mgz/download?version=4,g  ../Helper/BiomarkerBenchmark/download.sh > download.sh
