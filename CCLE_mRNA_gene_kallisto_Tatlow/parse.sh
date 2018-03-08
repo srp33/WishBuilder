64ee1eaeeed31ce6b04a958e0c7607ecd0df34b8
glenrs <grexsumsion@gmail.com>
2018-03-08 15:55:22 -0700

    added drug data

--- CCLE_mRNA_gene_kallisto_Tatlow/parse.sh
+++ CCLE_mRNA_gene_kallisto_Tatlow/parse.sh
@@ -3,6 +3,8 @@
 redirectedTempFolder=tmp
 expressionData=$redirectedTempFolder/CCLE_tpm_Expression.tsv
 clinicalAnnotations=$redirectedTempFolder/CCLE_tpm_Clinical.tsv
+drugData=$redirectedTempFolder/CCLE_NP24.2009_Drug_data_2015.02.24.csv
+profilingData=$redirectedTempFolder/CCLE_NP24.2009_profiling_2012.02.20.csv
 dataOutFile=data.tsv
 metadataOutFile=metadata.tsv
 dataOutFilegz=data.tsv.gz
@@ -11,7 +13,7 @@ metadataOutFilegz=metadata.tsv.gz
 rm -f $metadataOutFilegz
 rm -f $dataOutFilegz
 
-python parse.py $expressionData $clinicalAnnotations $dataOutFile $metadataOutFile
+python parse.py $expressionData $clinicalAnnotations $dataOutFile $metadataOutFile $drugData $profilingData
 
 gzip $metadataOutFile
 gzip $dataOutFile
