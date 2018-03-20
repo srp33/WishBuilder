ba42573a27d8b0314b251a902742d3508021d90c
Benjamin Cookson <btc36@m8int02.fsl.byu.edu>
2018-03-20 17:12:19 -0600

    Wrote parse code

--- TCGA_BreastCancer_CNV/download.sh
+++ TCGA_BreastCancer_CNV/download.sh
@@ -1,20 +1,17 @@
 #! /bin/bash
 
 redirectedTemp=tmp
-inFiles=$redirectedTemp/inFiles
 mkdir -p $redirectedTemp
-mkdir -p $inFiles
 
-cd $inFiles
 
-url="https://api.gdc.cancer.gov/data/c6a029e5-0ea3-410d-9e67-360bdfee2914"
-fileName=afile
+url="https://tcga.xenahubs.net/download/TCGA.BRCA.sampleMap/Gistic2_CopyNumber_Gistic2_all_thresholded.by_genes.gz"
+fileName=$redirectedTemp/Gistic2_CopyNumber_Gistic2_all_thresholded.by_genes.gz
 wget -O $fileName $url
 gunzip $fileName
 
 #downloading CancerType Samples
 url="https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE62944&format=file&file=GSE62944%5F06%5F01%5F15%5FTCGA%5F24%5FCancerType%5FSamples%2Etxt%2Egz"
-fileName=GSE62944_06_01_15_TCGA_24_CancerType_Samples.txt.gz
+fileName=$redirectedTemp/GSE62944_06_01_15_TCGA_24_CancerType_Samples.txt.gz
 
 wget -O $fileName $url
 gunzip $fileName
