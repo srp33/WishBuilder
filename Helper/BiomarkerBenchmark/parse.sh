6544394c2b252687c4a7c1577cddabe06eca7df6
Daniel Larson <dzlarson332@gmail.com>
2018-06-06 12:28:03 -0600

    Addition of translate.py to helper file of biobenchmark and adjustments to code to include it in install and parse scripts

--- Helper/BiomarkerBenchmark/parse.sh
+++ Helper/BiomarkerBenchmark/parse.sh
@@ -7,11 +7,15 @@ dataOutFile=data.tsv
 metadataOutFile=metadata.tsv
 dataOutFilegz=data.tsv.gz
 metadataOutFilegz=metadata.tsv.gz
+metadataOutTranslated=Clinical.tsv.gz
 
 rm -f $metadataOutFilegz
 rm -f $dataOutFilegz
+rm -f $metadataOutTranslated
 
 python parse.py $Clinical $Expression $dataOutFile $metadataOutFile 
 
 gzip $metadataOutFile
 gzip $dataOutFile
+
+python3 translate.py $metadataOutFilegz $metadataOutTranslated
