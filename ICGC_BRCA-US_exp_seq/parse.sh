728efb26f2a6581a97c97f2dd4508d40daf9f901
Daniel Larson <dzlarson332@gmail.com>
2018-06-20 08:54:32 -0600

    Modification of install and parse scripts to include convertTallFormatToWide.py

--- ICGC_BRCA-US_exp_seq/parse.sh
+++ ICGC_BRCA-US_exp_seq/parse.sh
@@ -6,3 +6,5 @@ set -euo pipefail
 python3 parse_metadata.py "donor.BRCA-US.tsv.gz" "metadata.tsv.gz"
 python3 parse_data.py "exp_seq.BRCA-US.tsv.gz" "data.tsv.gz"
 python3 keep_common_samples.py "metadata.tsv.gz" "data.tsv.gz"
+python3 convertTallFormatToWide.py "metadata.tsv.gz" "Clinical.tsv.gz"
+
