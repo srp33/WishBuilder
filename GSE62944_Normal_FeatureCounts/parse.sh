4a3536cdcf2ba2a98534aa5f19eeff51b8ba0d85
srp33 <stephen_piccolo@byu.edu>
2018-08-17 14:54:18 -0600

    Cleaning up and troubleshooting error with missing test_metadata.tsv

--- GSE62944_Normal_FeatureCounts/parse.sh
+++ GSE62944_Normal_FeatureCounts/parse.sh
@@ -3,13 +3,10 @@
 redirectedTempFolder=tmp
 PatientCancerType=$redirectedTempFolder/GSE62944_06_01_15_TCGA_24_Normal_CancerType_Samples.txt.gz
 NormalFeatureCounts=$redirectedTempFolder/GSM1697009_06_01_15_TCGA_24.normal_Rsubread_FeatureCounts.txt.gz
-tcgaHtml=$redirectedTempFolder/"tcga_abbreviations.html"
 nameToAbbreviation="nameToAbbreviation.txt"
 dataOutFilegz=data.tsv.gz
 metadataOutFilegz=metadata.tsv.gz
 clinicalDataOut=Clinical.tsv.gz
-
-#source activate WishBuilderDependencies
 
 python parse.py $PatientCancerType $NormalFeatureCounts $dataOutFilegz $metadataOutFilegz $nameToAbbreviation
 python convertTallFormatToWide.py $metadataOutFilegz $clinicalDataOut
