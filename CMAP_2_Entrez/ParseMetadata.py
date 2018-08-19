19c75935dba19e48bf66316b52bd8f439f404ac3
srp33 <stephen_piccolo@byu.edu>
2018-08-18 22:03:49 -0600

    Fixed some bugs in parsing metadata

--- CMAP_2_Entrez/ParseMetadata.py
+++ CMAP_2_Entrez/ParseMetadata.py
@@ -4,19 +4,19 @@ import gzip
 metaDataIn = argv[1]
 metaDataOut = argv[2]
 
-with gzip.open(metaDataIn, 'r') as mdI:
+with open(metaDataIn, 'r') as mdI:
     with gzip.open(metaDataOut, 'w') as mdO:
-        headerItems = mdI.readline().decode().strip("\n").split("\t")
+        headerItems = mdI.readline().strip("\n").split("\t")
 
         # Remove last column name and put "Sample" at the beginning
         headerItems = ["Sample"] + headerItems[:-1]
 
-        mdO.write(("Sample\tVariable\tValue\n").encode())
+        mdO.write(("\t".join(headerItems) + "\n").encode())
 
         for line in mdI:
-            lineItems = line.decode().strip('\n').split('\t')
+            lineItems = line.strip('\n').split('\t')
 
             # Remove last item and put it at the beginning
             lineItems = [lineItems[-1]] + lineItems[:-1]
 
-            md0.write(("\t".join(lineItems) + "\n").encode())
+            mdO.write(("\t".join(lineItems) + "\n").encode())
