015b37e6f8de6f82a43af6fffa50e2710ff46cf2
srp33 <stephen_piccolo@byu.edu>
2018-08-18 20:20:38 -0600

    Uncommented some lines that shouldn't have been commented.

--- CMAP_2_Entrez/parse.sh
+++ CMAP_2_Entrez/parse.sh
@@ -4,7 +4,7 @@ dataIn=tmp/Matrices/CMap_SCAN_EntrezGene_BatchAdjusted.tsv.gz
 convertedData=tmp/test.tsv
 metaDataIn=tmp/CMAP_Metadata.tsv
 
-#python3 parse.py $convertedData $metaDataIn $dataOut tmp/Metadata.tsv.gz
-#python3 convertTallFormatToWide.py tmp/Metadata.tsv.gz Metadata.tsv.gz
+python3 parse.py $convertedData $metaDataIn $dataOut tmp/Metadata.tsv.gz
+python3 convertTallFormatToWide.py tmp/Metadata.tsv.gz Metadata.tsv.gz
 
 Rscript entrezIdConvert.R tmp/Matrices/CMap_SCAN_EntrezGene_BatchAdjusted.tsv.gz Gene_Expression.tsv.gz
