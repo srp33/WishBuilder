5170375d41bf4329d9b4c3f352433c8dd2a83567
srp33 <stephen_piccolo@byu.edu>
2018-08-18 22:04:40 -0600

    Removed unnecessary comment character

--- CMAP_2_Entrez/parse.sh
+++ CMAP_2_Entrez/parse.sh
@@ -2,4 +2,4 @@
 
 python3 ParseMetadata.py tmp/CMAP_Metadata.tsv Metadata.tsv.gz
 
-#Rscript entrezIdConvert.R tmp/Matrices/CMap_SCAN_EntrezGene_BatchAdjusted.tsv.gz Gene_Expression.tsv.gz
+Rscript entrezIdConvert.R tmp/Matrices/CMap_SCAN_EntrezGene_BatchAdjusted.tsv.gz Gene_Expression.tsv.gz
