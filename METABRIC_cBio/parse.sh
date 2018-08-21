e359d5b67f7c1a367a1eaf68d9946514d7fb170a
srp33 <stephen_piccolo@byu.edu>
2018-08-20 21:51:43 -0600

    Running some tests

--- METABRIC_cBio/parse.sh
+++ METABRIC_cBio/parse.sh
@@ -2,9 +2,10 @@
 
 set -euo pipefail
 
-python3 ParseClinical.py "tmp/data_clinical_sample.txt" "tmp/data_clinical_patient.txt" "tmp/data_CNA.txt" "tmp/data_mutations_extended.txt" "tmp/Clinical.tsv.gz"
+python3 ParseClinical.py "tmp/data_clinical_sample.txt" "tmp/Clinical_Patient.tsv.gz"
+#python3 ParseClinical.py "tmp/data_clinical_sample.txt" "tmp/data_clinical_patient.txt" "tmp/data_CNA.txt" "tmp/data_mutations_extended.txt" "tmp/Clinical.tsv.gz"
 
-python3 ParseExpression.py "tmp/data_expression.txt" "Gene_Expression.tsv.gz"
+#python3 ParseExpression.py "tmp/data_expression.txt" "Gene_Expression.tsv.gz"
 
-python3 keep_common_samples.py "tmp/Clinical.tsv.gz" "Gene_Expression.tsv.gz"
-python3 convertTallFormatToWide.py "tmp/Clinical.tsv.gz" "Clinical.tsv.gz"
+#python3 keep_common_samples.py "tmp/Clinical.tsv.gz" "Gene_Expression.tsv.gz"
+#python3 convertTallFormatToWide.py "tmp/Clinical.tsv.gz" "Clinical.tsv.gz"
