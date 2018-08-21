09586f3119ebb23969bcb40d234e33c0a574d1b3
srp33 <stephen_piccolo@byu.edu>
2018-08-20 21:56:47 -0600

    Running some tests

--- METABRIC_cBio/parse.sh
+++ METABRIC_cBio/parse.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-python3 ParseClinical.py "tmp/data_clinical_sample.txt" "tmp/Clinical_Patient.tsv.gz"
+python3 ParseClinical.py "tmp/data_clinical_patient.txt" "tmp/Clinical_Patient.tsv.gz"
 #python3 ParseClinical.py "tmp/data_clinical_sample.txt" "tmp/data_clinical_patient.txt" "tmp/data_CNA.txt" "tmp/data_mutations_extended.txt" "tmp/Clinical.tsv.gz"
 
 #python3 ParseExpression.py "tmp/data_expression.txt" "Gene_Expression.tsv.gz"
