21818ec64372ff3c95595cc6ce06e37529b4c1dc
srp33 <stephen_piccolo@byu.edu>
2018-08-22 06:08:48 -0600

    Testing overall system

--- METABRIC_cBio/download.sh
+++ METABRIC_cBio/download.sh
@@ -19,7 +19,8 @@ function downloadData {
 
 mkdir -p tmp
 
-for f in data_clinical_patient.txt data_clinical_sample.txt data_CNA.txt data_mutations_extended.txt data_expression.txt
+#for f in data_clinical_patient.txt data_clinical_sample.txt data_CNA.txt data_mutations_extended.txt data_expression.txt
+for f in data_clinical_patient.txt data_clinical_sample.txt data_mutations_extended.txt
 do
   downloadData https://media.githubusercontent.com/media/cBioPortal/datahub/master/public/brca_metabric/$f
 done
