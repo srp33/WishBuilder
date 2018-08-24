4b3ba7725c417ca756562a3229b622c38028573b
srp33 <stephen_piccolo@byu.edu>
2018-08-24 14:10:21 -0600

    Fixed inconsistent test data; fixed gene-expression output file so that the first column header is Sample

--- GSE62944_Tumor_FeatureCounts/parse.sh
+++ GSE62944_Tumor_FeatureCounts/parse.sh
@@ -4,4 +4,5 @@ python3 ParseCancerType.py tmp/GSE62944_06_01_15_TCGA_24_CancerType_Samples.txt.
 python3 ParseClinical.py tmp/GSE62944_06_01_15_TCGA_24_548_Clinical_Variables_9264_Samples.txt.gz tmp/Clinical.tsv.gz
 python3 convertTallFormatToWide.py tmp/Clinical.tsv.gz Clinical.tsv.gz
 
-python3 TransposeData.py tmp/GSM1536837_06_01_15_TCGA_24.tumor_Rsubread_FeatureCounts.txt.gz Gene_Expression.tsv.gz
+python3 TransposeData.py tmp/GSM1536837_06_01_15_TCGA_24.tumor_Rsubread_FeatureCounts.txt.gz tmp/Gene_Expression.tsv.gz
+python3 SetFirstColumnHeader.py tmp/Gene_Expression.tsv.gz Sample Gene_Expression.tsv.gz
