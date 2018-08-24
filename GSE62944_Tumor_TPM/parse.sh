3c9d9a497d7d680a1dc0b1caa6dc6054e6545a43
srp33 <stephen_piccolo@byu.edu>
2018-08-24 13:45:08 -0600

    Uncommented lines

--- GSE62944_Tumor_TPM/parse.sh
+++ GSE62944_Tumor_TPM/parse.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-#python3 ParseCancerType.py tmp/GSE62944_06_01_15_TCGA_24_CancerType_Samples.txt.gz nameToAbbreviation.txt tmp/Clinical.tsv.gz
-#python3 ParseClinical.py tmp/GSE62944_06_01_15_TCGA_24_548_Clinical_Variables_9264_Samples.txt.gz tmp/Clinical.tsv.gz
-#python3 convertTallFormatToWide.py tmp/Clinical.tsv.gz Clinical.tsv.gz
+python3 ParseCancerType.py tmp/GSE62944_06_01_15_TCGA_24_CancerType_Samples.txt.gz nameToAbbreviation.txt tmp/Clinical.tsv.gz
+python3 ParseClinical.py tmp/GSE62944_06_01_15_TCGA_24_548_Clinical_Variables_9264_Samples.txt.gz tmp/Clinical.tsv.gz
+python3 convertTallFormatToWide.py tmp/Clinical.tsv.gz Clinical.tsv.gz
 
 python3 TransposeData.py tmp/GSM1536837_06_01_15_TCGA_24.tumor_Rsubread_TPM.txt.gz Gene_Expression.tsv.gz
