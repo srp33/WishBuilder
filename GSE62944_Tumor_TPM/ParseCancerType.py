3439b19cb2962b704c57d2cfc4b9a9d57a96ad11
srp33 <stephen_piccolo@byu.edu>
2018-08-24 13:33:43 -0600

    Simplified data processing

---
+++ GSE62944_Tumor_TPM/ParseCancerType.py
@@ -0,0 +1,26 @@
+import sys, gzip
+
+inFilePath = sys.argv[1]
+abbrevFilePath = sys.argv[2]
+outFilePath = sys.argv[3]
+
+abbrevDict = {}
+with open(abbrevFilePath) as abbrevFile:
+    for line in abbrevFile:
+        lineList = line.strip('\n').split('\t')
+        longerName = lineList[0]
+        abbrev = lineList[1]
+        abbrevDict[abbrev] = longerName
+
+with gzip.open(inFilePath, 'r') as inFile:
+    with gzip.open(outFilePath, 'w') as outFile:
+        outFile.write(("\t".join(["Sample","Variable","Value"]) + "\n").encode())
+
+        for line in inFile:
+            lineList = line.decode().strip('\n').split('\t')
+            #sampleID = lineList[0][:15]
+            sampleID = lineList[0]
+            cancerTypeAbbrev = lineList[1]
+            cancerType = abbrevDict[cancerTypeAbbrev]
+
+            outFile.write(("\t".join([sampleID, "Cancer Type", cancerType]) + "\n").encode())
--- GSE62944_Tumor_TPM/parse.sh
+++ GSE62944_Tumor_TPM/parse.sh
@@ -1,20 +1,7 @@
 #! /bin/bash
 
-redirectedTempFolder=tmp
-metaData=$redirectedTempFolder/GSE62944_06_01_15_TCGA_24_548_Clinical_Variables_9264_Samples.txt.gz
-patientCancerType=$redirectedTempFolder/GSE62944_06_01_15_TCGA_24_CancerType_Samples.txt.gz
-tumorTPM=$redirectedTempFolder/GSM1536837_06_01_15_TCGA_24.tumor_Rsubread_TPM.txt.gz
-tcgaHtml=$redirectedTempFolder/"tcga_abbreviations.html"
-nameToAbbreviation=$redirectedTempFolder/"nameToAbbreviation.txt"
-dataOut=data.tsv.gz
-metadataOut=metadata.tsv.gz
-metaDataConverted=Clinical.tsv.gz
-#source activate WishBuilderDependencies
+#python3 ParseCancerType.py tmp/GSE62944_06_01_15_TCGA_24_CancerType_Samples.txt.gz nameToAbbreviation.txt tmp/Clinical.tsv.gz
+#python3 ParseClinical.py tmp/GSE62944_06_01_15_TCGA_24_548_Clinical_Variables_9264_Samples.txt.gz tmp/Clinical.tsv.gz
+#python3 convertTallFormatToWide.py tmp/Clinical.tsv.gz Clinical.tsv.gz
 
-Rscript scrapeWebTCGA.R $tcgaHtml $nameToAbbreviation
-python3 parse.py $metaData $patientCancerType $tumorTPM $dataOut $metadataOut $nameToAbbreviation
-python3 translate.py $metadataOut $metaDataConverted
-<<<<<<< HEAD
-=======
-
->>>>>>> 882b5bc7f15b51d8e83e55a4df86e4dd972436d8
+python3 TransposeData.py tmp/GSM1536837_06_01_15_TCGA_24.tumor_Rsubread_TPM.txt.gz Gene_Expression.tsv.gz
