f92ee79f37342e45b703220288ccf2f6d1270ed1
srp33 <stephen_piccolo@byu.edu>
2018-08-24 18:38:47 -0600

    Troubleshooting weird XML installation error

--- GSE62944_Tumor_TPM/parse.sh
+++ GSE62944_Tumor_TPM/parse.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+echo "got here!!!!!!!!!!!!!!!!!!!"
+
 python3 ParseCancerType.py tmp/GSE62944_06_01_15_TCGA_24_CancerType_Samples.txt.gz nameToAbbreviation.txt tmp/Clinical.tsv.gz
 python3 ParseClinical.py tmp/GSE62944_06_01_15_TCGA_24_548_Clinical_Variables_9264_Samples.txt.gz tmp/Clinical.tsv.gz
 python3 convertTallFormatToWide.py tmp/Clinical.tsv.gz Clinical.tsv.gz
