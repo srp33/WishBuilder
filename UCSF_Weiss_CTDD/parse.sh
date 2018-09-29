481efa2723b7340551bd35e574ab8d380c41a629
srp33 <stephen_piccolo@byu.edu>
2018-09-28 22:41:04 -0600

    Renamed test files

--- UCSF_Weiss_CTDD/parse.sh
+++ UCSF_Weiss_CTDD/parse.sh
@@ -3,6 +3,6 @@
 set -euo pipefail
 
 python3 parse_data.py tmp/expression_main.txt tmp/probe_attributes.txt Gene_Expression.tsv
-python3 parse_metadata.py tmp/sample_attributes.txt tmp/Clinical.tsv
-python3 keep_common_samples.py tmp/Clinical.tsv Gene_Expression.tsv
-python3 ../Helper/convertTallFormatToWide2.py tmp/Clinical.tsv Clinical.tsv
+python3 parse_metadata.py tmp/sample_attributes.txt tmp/Metadata.tsv
+python3 keep_common_samples.py tmp/Metadata.tsv Gene_Expression.tsv
+python3 ../Helper/convertTallFormatToWide2.py tmp/Metadata.tsv Metadata.tsv
