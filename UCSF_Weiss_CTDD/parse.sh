bb226ab839bc7a7c22979f376fe616aa18759616
srp33 <stephen_piccolo@byu.edu>
2018-09-28 22:27:35 -0600

    Changed from .tsv.gz to .tsv

--- UCSF_Weiss_CTDD/parse.sh
+++ UCSF_Weiss_CTDD/parse.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-#python3 parse_data.py "tmp/expression_main.txt" "tmp/probe_attributes.txt" "data.tsv"
-#python3 parse_metadata.py "tmp/sample_attributes.txt" "metadata.tsv"
-#python3 keep_common_samples.py "metadata.tsv" "data.tsv"
+python3 parse_data.py "tmp/expression_main.txt" "tmp/probe_attributes.txt" "data.tsv"
+python3 parse_metadata.py "tmp/sample_attributes.txt" "metadata.tsv"
+python3 keep_common_samples.py "metadata.tsv" "data.tsv"
 python3 ../Helper/convertTallFormatToWide2.py "metadata.tsv" "Clinical.tsv"
