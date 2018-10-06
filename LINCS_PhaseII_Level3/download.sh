bbe52e9eafc2a63a192da8f416adc36794cc9dcf
srp33 <stephen_piccolo@byu.edu>
2018-10-06 15:59:44 -0600

    Troubleshooting error with decoding in compare.py

--- LINCS_PhaseII_Level3/download.sh
+++ LINCS_PhaseII_Level3/download.sh
@@ -17,5 +17,5 @@ downloadFile "https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE70138&format=fil
 downloadFile "https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE70138&format=file&file=GSE70138%5FBroad%5FLINCS%5Fcell%5Finfo%5F2017%2D04%2D28%2Etxt%2Egz" "GSE70138_Broad_LINCS_cell_info_2017-04-28.txt.gz" 
 downloadFile "https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE70138&format=file&file=GSE70138%5FBroad%5FLINCS%5Fpert%5Finfo%5F2017%2D03%2D06%2Etxt%2Egz" "GSE70138_Broad_LINCS_pert_info_2017-03-06.txt.gz" 
 
-downloadFile "https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE70138&format=file&file=GSE70138%5FBroad%5FLINCS%5FLevel3%5FINF%5Fmlr12k%5Fn345976x12328%5F2017%2D03%2D06%2Egctx%2Egz" "LINCS_PhaseII_Level3.gctx.gz" 
-gunzip "tmp/LINCS_PhaseII_Level3.gctx.gz"
+#downloadFile "https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE70138&format=file&file=GSE70138%5FBroad%5FLINCS%5FLevel3%5FINF%5Fmlr12k%5Fn345976x12328%5F2017%2D03%2D06%2Egctx%2Egz" "LINCS_PhaseII_Level3.gctx.gz" 
+#gunzip "tmp/LINCS_PhaseII_Level3.gctx.gz"
--- LINCS_PhaseII_Level3/parse.sh
+++ LINCS_PhaseII_Level3/parse.sh
@@ -5,4 +5,4 @@ set -o errexit
 python ParseMetadata.py tmp/GSE70138_Broad_LINCS_inst_info.txt.gz tmp/GSE70138_Broad_LINCS_cell_info_2017-04-28.txt.gz tmp/GSE70138_Broad_LINCS_pert_info_2017-03-06.txt.gz tmp/Metadata.tsv
 python ../Helper/convertTallFormatToWide2.py tmp/Metadata.tsv Metadata.tsv
 
-python ParseExpression.py tmp/LINCS_PhaseII_Level3.gctx tmp/GSE70138_Broad_LINCS_gene_info_2017-03-06.txt.gz Gene_Expression.tsv
+#python ParseExpression.py tmp/LINCS_PhaseII_Level3.gctx tmp/GSE70138_Broad_LINCS_gene_info_2017-03-06.txt.gz Gene_Expression.tsv
