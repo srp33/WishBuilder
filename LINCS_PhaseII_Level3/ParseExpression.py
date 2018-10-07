2a5d3bad7b2636e038269a67f57c2e1d3a68db92
srp33 <stephen_piccolo@byu.edu>
2018-10-06 19:44:30 -0600

    Removed gzipping

--- LINCS_PhaseII_Level3/ParseExpression.py
+++ LINCS_PhaseII_Level3/ParseExpression.py
@@ -9,11 +9,9 @@ dataOut = sys.argv[3]
 f = h5py.File(gctxFileName, "r")
 
 grpname = f.require_group('/0')
-
 subgrpMeta = grpname.require_group('/0/META')
 colgrp = subgrpMeta.require_group('/0/META/ROW')
 rowgrp = subgrpMeta.require_group('/0/META/COL')
-
 subgrpData = grpname.require_group('/0/DATA')
 subsubgrpData = subgrpData.require_group('/0/DATA/0')
 
@@ -25,27 +23,27 @@ with gzip.open(geneFile, 'r') as f :
         list = line.decode().strip('\n').split('\t')
         geneDict[list[0]] = list[1]
 
-print("writing expression file")
+print("Writing expression file")
+f = open(dataOut, 'w', encoding="utf-8")
 
-f = gzip.open(dataOut, 'w')
 try :
-    f.write("Sample".encode())
+    f.write("Sample")
     for value in colgrp["id"] :
-        f.write(('\t' + geneDict[str(int(value))]).encode())
-    f.write('\n'.encode())
+        f.write('\t' + geneDict[str(int(value))])
+    f.write('\n')
 
     index = 0
     for line in subsubgrpData["matrix"] :
         a = np.asarray(line).astype(str)
-        value = rowgrp["id"][index].decode()
-        f.write((value + '\t' + '\t'.join(a) + '\n').encode())
+        number = rowgrp["id"][index].decode()
+
+        f.write(number + '\t' + '\t'.join(a) + '\n')
         index = index + 1
 
-#        if index == 100:
-#            break
-
-        if index % 1000 == 0:
-            print(str(index) + " of 345976 expression data")
+        if index % 1000 == 0 :
+            print(str(index) + " expression rows")
             sys.stdout.flush()
+            #break
+
 finally :
     f.close()
