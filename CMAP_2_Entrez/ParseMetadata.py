8a83a9bd96337fc4a4849bc4564786e3aac5629d
Alyssa Claire Parker <parkerac@m8int02.fsl.byu.edu>
2018-10-09 10:51:20 -0600

    Update CMAP_2_Entrez output to normal text files

--- CMAP_2_Entrez/ParseMetadata.py
+++ CMAP_2_Entrez/ParseMetadata.py
@@ -5,13 +5,13 @@ metaDataIn = argv[1]
 metaDataOut = argv[2]
 
 with open(metaDataIn, 'r') as mdI:
-    with gzip.open(metaDataOut, 'w') as mdO:
+    with open(metaDataOut, 'w') as mdO:
         headerItems = mdI.readline().strip("\n").split("\t")
 
         # Remove last column name and put "Sample" at the beginning
         headerItems = ["Sample"] + headerItems[:-1]
 
-        mdO.write(("\t".join(headerItems) + "\n").encode())
+        mdO.write(("\t".join(headerItems) + "\n"))
 
         for line in mdI:
             lineItems = line.strip('\n').split('\t')
@@ -19,4 +19,4 @@ with open(metaDataIn, 'r') as mdI:
             # Remove last item and put it at the beginning
             lineItems = [lineItems[-1]] + lineItems[:-1]
 
-            mdO.write(("\t".join(lineItems) + "\n").encode())
+            mdO.write(("\t".join(lineItems) + "\n"))
--- CMAP_2_Entrez/parse.sh
+++ CMAP_2_Entrez/parse.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-python3 ParseMetadata.py tmp/CMAP_Metadata.tsv Metadata.tsv.gz
+python3 ParseMetadata.py tmp/CMAP_Metadata.tsv Metadata.tsv
 
-Rscript entrezIdConvert.R tmp/Matrices/CMap_SCAN_EntrezGene_BatchAdjusted.tsv.gz Gene_Expression.tsv.gz
+Rscript entrezIdConvert.R tmp/Matrices/CMap_SCAN_EntrezGene_BatchAdjusted.tsv.gz Gene_Expression.tsv
