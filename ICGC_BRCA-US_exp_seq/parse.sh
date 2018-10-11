44d9cd7ec614e39f9997b5313876c061c7a56e11
Alyssa Claire Parker <parkerac@m8int02.fsl.byu.edu>
2018-10-11 08:37:38 -0600

    Remove metadata.tsv after the information is reformatted and saved to Clinical.tsv

--- ICGC_BRCA-US_exp_seq/parse.sh
+++ ICGC_BRCA-US_exp_seq/parse.sh
@@ -7,4 +7,4 @@ python3 parse_metadata.py tmp/donor.BRCA-US.tsv.gz "metadata.tsv"
 python3 parse_data.py tmp/exp_seq.BRCA-US.tsv.gz "Gene_Expression.tsv"
 python3 keep_common_samples.py "metadata.tsv" "Gene_Expression.tsv"
 python3 ../Helper/convertTallFormatToWide2.py "metadata.tsv" "Clinical.tsv"
-
+rm -f "metadata.tsv"
