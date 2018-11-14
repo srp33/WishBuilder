c1b53e6b2f74711db3cb2f234bb76afcf9769138
srp33 <stephen_piccolo@byu.edu>
2018-11-13 17:15:58 -0700

    Added script to convert add gene symbols to Ensemble IDs.

--- Helper/convertEnsemblGeneIDs.R
+++ Helper/convertEnsemblGeneIDs.R
@@ -5,19 +5,19 @@ library(biomaRt)
 
 args = commandArgs(trailingOnly=TRUE)
 
-#Remove sampleID column from data and place in own vector
+print("Remove sampleID column from data and place in own vector")
 data <- read_tsv(args[1])
 sampleIDs <- pull(data, Sample)
 data <- dplyr::select(data, -Sample)
 
-#Get ensemblIDs from data and place in vector
+print("Get ensemblIDs from data and place in vector")
 genes <- colnames(data)
 
-#Get gene symbols that correspond with ensemblIDs
+print("Get gene symbols that correspond with ensemblIDs")
 mart <- useDataset("hsapiens_gene_ensembl", useMart("ensembl"))
 geneSymbols <- getBM(filters="ensembl_gene_id", attributes = c("ensembl_gene_id", "hgnc_symbol"), values = genes, mart=mart)
 
-#Combine symbols and IDs
+print("Combine symbols and IDs")
 symbolsAndIDs <- c()
 for (i in 1:length(genes)) {
   geneSymbol <- geneSymbols$hgnc_symbol[grepl(genes[i], geneSymbols$ensembl_gene_id)]
@@ -29,18 +29,18 @@ for (i in 1:length(genes)) {
   }
 }
 
-#Create list of NA symbol indices (basically those that are missing from the online database)
+print("Create list of NA symbol indices (basically those that are missing from the online database)")
 missingSymbols <- which(is.na(symbolsAndIDs))
 
-#Remove columns with missing symbols from data and list of GeneSymbols
+print("Remove columns with missing symbols from data and list of GeneSymbols")
 if (length(missingSymbols > 0)) {
   data <- data[,-missingSymbols]
   symbolsAndIDs <- symbolsAndIDs[-missingSymbols]
 }
 
-#Add sampleIDs column to front of data
+print("Add sampleIDs column to front of data")
 data <- cbind(sampleIDs, data)
 
-#Replace ensembl ids in data with genesymbols
+print("Replace Ensembl ids in data with gene symbols")
 colnames(data) <- c("Sample", symbolsAndIDs)
 write_tsv(data, args[2])
