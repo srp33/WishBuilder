d4878732f64961822c3baa0ebd76864a3dd69219
srp33 <stephen_piccolo@byu.edu>
2019-04-11 08:02:52 -0600

    Specifying Ensembl mirror

--- Helper/mapEnsemblGeneToHGNC.R
+++ Helper/mapEnsemblGeneToHGNC.R
@@ -9,9 +9,10 @@ dataPeek = read_tsv(args[1], n_max=0)
 geneIDs = colnames(dataPeek)[2:ncol(dataPeek)]
 
 #mart <- useDataset("hsapiens_gene_ensembl", useMart("ensembl"))
-mart <- useMart(biomart = "ENSEMBL_MART_ENSEMBL", 
-                dataset = "hsapiens_gene_ensembl", 
-                host = "www.ensembl.org")
+#mart <- useMart(biomart = "ENSEMBL_MART_ENSEMBL", 
+#                dataset = "hsapiens_gene_ensembl", 
+#                host = "www.ensembl.org")
+mart = useEnsembl(biomart = "ENSEMBL_MART_ENSEMBL", dataset = "hsapiens_gene_ensembl", host, mirror = "uswest", verbose = TRUE)
 
 geneSymbols <- getBM(filters="ensembl_gene_id", attributes = c("ensembl_gene_id", "hgnc_symbol"), values = geneIDs, mart=mart)
 
