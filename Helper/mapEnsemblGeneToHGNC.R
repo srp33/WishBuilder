f80fd77c5baca67b83bb0e0b4637111b44433c56
srp33 <stephen_piccolo@byu.edu>
2019-04-13 07:27:23 -0600

    Reworked logic for saving alias file

--- Helper/mapEnsemblGeneToHGNC.R
+++ Helper/mapEnsemblGeneToHGNC.R
@@ -13,7 +13,7 @@ mart = useEnsembl(biomart = "ENSEMBL_MART_ENSEMBL", dataset = "hsapiens_gene_ens
 geneSymbols = getBM(filters="ensembl_gene_id", attributes = c("ensembl_gene_id", "hgnc_symbol"), values = geneIDs, mart=mart)
 
 symbolsAndIDs = c()
-symbols = c()
+aliasData = NULL
 for (i in 1:length(geneIDs)) {
   ensembl_ID = geneIDs[i]
 
@@ -22,11 +22,11 @@ for (i in 1:length(geneIDs)) {
 
   if (length(matching_indices) == 0 || geneSymbol == "") {
     symbolsAndIDs[i] = ensembl_ID
-    symbols = ""
-    }
+  }
   else {
-    symbolsAndIDs[i] = paste0(geneSymbol, " (", ensembl_ID, ")") 
-    symbols = geneSymbol
+    symbolAndID = paste0(geneSymbol, " (", ensembl_ID, ")")
+    symbolsAndIDs[i] = symbolAndID
+    aliasData = rbind(aliasData, c(symbolAndID, geneSymbol))
   }
 }
 
